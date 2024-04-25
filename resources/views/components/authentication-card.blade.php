71029a9dbe27214a51673c06b1c3aa05934523a8
Manuelo69 <ichigo2418@gmail.com>
2024-04-25 20:23:04 +0200

    estilo general registro hecho

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -3,7 +3,6 @@
         class="w-full sm:max-w-md mt-6 px-6 py-4 bg-azul border-2 border-black shadow-lg shadow-white overflow-hidden sm:rounded-lg">
         {{ $logo }}
 
-        >
         {{ $slot }}
     </div>
 </div>
